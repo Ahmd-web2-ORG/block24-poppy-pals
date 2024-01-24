d49c1fd554bc59b55b5d9e2b0c909dbed4fae23a
Your Name <ahmed9@Ahmeds-Air.lan>
2024-01-23 21:26:57 -0500

    add jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      hellow Edwin is great man
+    </div>
+  )
+}
+
+export default App
