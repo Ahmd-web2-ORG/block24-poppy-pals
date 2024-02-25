72c20a51ecdd18ba5d2763d6caba9207a321e02a
alabdrah9 <142067927+alabdrah9@users.noreply.github.com>
2024-02-25 08:25:23 -0500

    Update App.jsx
    do this missing part:  rendering the puppy list. Now you just need to render individual puppy info when a puppy is clicked.

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,31 @@ import {puppyList} from './data.js'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
   const [featPupId, setFeatPupId] = useState(null);
-const featuredPup = puppies.find((puppy)=> puppy.id === featPupId)
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup);
 
+  function handleClick(puppy) {
+    console.log("puppy id: ", puppy.id);
+    setFeatPupId(puppy.id);
+  }
   return (
-    <div className="App">
-      {puppies.map((puppy) => {
-        return (
-          <p onClick={()=> setFeatPupId(puppy.id)} key={puppy.id}>
-            {puppy.name}
-          </p>
-        );
-      })}
-
-{featPupId && (
-        <div>
-          <h2>{featuredPup.name}</h2>
-          <ul>
-            <li>Age: {featuredPup.age}</li>
-            <li>Email: {featuredPup.email}</li>
-          </ul>
-        </div>
-      )}
-
+    <div className='App'>
+      {
+        puppies.map((puppy) => {
+          return <p onClick={() => handleClick(puppy)} key={puppy.id}>{puppy.name}</p>
+        })
+      }
+      {featPupId && <div>
+        <h2>{featuredPup.name}</h2>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+        </ul>
+      </div>}
     </div>
-  );
+  )
 }
 
 export default App
